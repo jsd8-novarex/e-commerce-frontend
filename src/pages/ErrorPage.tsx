4358d41e3f6fd7196c1f531bb1b729da1792baf5
Zombox <ghostzb@outlook.com>
2024-10-11 09:43:51 +0700

    add ErrorPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import { useNavigate } from "react-router";
 
 function ErrorPage() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>ErrorPage</h1>
+    <div className='grid h-dvh w-dvw grid-rows-6 p-4 text-xl sm:p-8 md:p-12'>
+      <span>404</span>
+      <div className='row-start-2 sm:row-start-3'>
+        <div className='my-8'>
+          <h1>Page not found</h1>
+        </div>
+        <p>The page you are looking for doesn't exist.</p>
+        <div className='mt-12'>
+          <button
+            type='button'
+            className='btn btn-primary w-24 border-black bg-black text-white hover:border-black hover:bg-black hover:text-white'
+            onClick={() => navigate("/")}
+          >
+            home
+          </button>
+        </div>
+      </div>
     </div>
   );
 }

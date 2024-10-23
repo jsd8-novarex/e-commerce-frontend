a390e307abbf024f748abc5941feba493f46b570
Zombox <ghostzb@outlook.com>
2024-10-23 17:54:35 +0700

    edit layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavigationBar from "./NavigationBar";
-import Footer from "./Footer";
+// import Footer from "./Footer";
 
 function Layout() {
   return (

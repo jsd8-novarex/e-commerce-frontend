940b26c13c088fbf566553ef307f144dc6691e62
sirinda <sirinda.bdev@gmail.com>
2024-10-23 18:09:59 +0700

    edit(mine:signin): adjust space

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -2,41 +2,44 @@ import React from "react";
 
 function SignInPage() {
   return (
-    <div className='flex justify-center'>
-      <form className='relative flex h-dvh w-dvw flex-col justify-center p-10 md:w-2/5'>
-        <div className='text-center text-xl'>Log in</div>
-        <div className='py-2.5'>
-          <input
-            type='email'
-            placeholder='Enter your email'
-            className='w-full border border-black'
-            required
-          ></input>
-        </div>
-        <div className='py-2.5'>
-          <input
-            type='password'
-            placeholder='Enter your password'
-            className='w-full border border-black'
-            required
-          ></input>
-        </div>
-        <div className='py-2.5'>
-          <input type='checkbox' className='border-black'></input>
-          <label>Remember me</label>
-        </div>
-        <div className='flex justify-center'>
-          <button type='submit' className='w-full bg-black text-white hover:bg-gray-600'>
-            LOGIN
-          </button>
-        </div>
-        <div className='text-right hover:text-gray-400'>
-          <a href=''>Forgot Password?</a>
-        </div>
-        <div className='py-2.5 text-center underline hover:text-gray-400'>
-          <a href=''>Create new account</a>
-        </div>
-      </form>
+    <div className='flex h-dvh w-dvw items-center justify-center bg-white md:bg-stone-100'>
+      <div className='flex h-fit w-dvw flex-col justify-center bg-white p-10 md:w-1/2 lg:w-1/3 xl:w-1/3'>        
+          <div className="py-5">
+            <h1 className='text-center text-4xl font-bold md:text-5xl'>Pangaea</h1>
+          </div>
+          <form>          
+          <div className='py-2.5'>
+            <input
+              type='email'
+              placeholder='Enter your email'
+              className='w-full border border-black px-2 h-8 md:h-10'
+              required
+            ></input>
+          </div>
+          <div className='py-2.5'>
+            <input
+              type='password'
+              placeholder='Enter your password'
+              className='w-full border border-black px-2 h-8 md:h-10 '
+              required
+            ></input>
+          </div>
+          <div className='flex justify-center'>
+            <button
+              type='submit'
+              className='w-full bg-black text-white hover:bg-gray-600 mt-2 h-8 md:h-10 md:text-xl'
+            >
+              LOGIN
+            </button>
+          </div>
+          </form>
+          <div className='text-right hover:text-gray-400 mt-2'>
+            <a href=''>Forgot Password?</a>
+          </div>
+          <div className='py-5 text-center underline hover:text-gray-400 md:text-xl mt-5'>
+            <a href=''>Create new account</a>
+          </div>        
+      </div>
     </div>
   );
 }

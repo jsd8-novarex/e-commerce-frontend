e07997eb9ddd1e99dfe6d05ef8745411abb9b3f4
Zombox <ghostzb@outlook.com>
2024-10-26 14:34:15 +0700

    fix(earth):height

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import { ShowCaseSectionData } from "../constraints/SHOWCASE_DATA";
 
 function HomePage() {
   return (
-    <main className='hide-scroll h-[91.5vh] w-dvw snap-y snap-mandatory overflow-hidden overflow-y-auto scroll-smooth ease-in-out'>
+    <main className='hide-scroll h-[91.7vh] w-dvw snap-y snap-mandatory overflow-hidden overflow-y-auto scroll-smooth ease-in-out'>
       {ShowCaseSectionData.map((data, index) => (
         <ShowcaseSection key={index} data={data} />
       ))}

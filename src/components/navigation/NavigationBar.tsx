ac48c12e4abde00158d52e196cef78cfdf0a390d
Zombox <ghostzb@outlook.com>
2024-10-29 00:17:36 +0700

    fix(earth/NavigationBar):move file and add component

---
+++ src/components/navigation/NavigationBar.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import HamburgerMenu from "./HamburgerMenu";
+import MobileMenu from "./MobileMenu";
+import DesktopLeftMenu from "./DesktopLeftMenu";
+import DesktopRightMenu from "./DesktopRightMenu";
+
+type NavigationBarPropsType = {
+  toggleCartSidebar: () => void;
+};
+
+function NavigationBar({ toggleCartSidebar }: NavigationBarPropsType) {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
+
+  return (
+    <nav className='sticky top-0 z-10'>
+      <div className='relative flex items-center justify-between bg-white px-4 py-5 md:px-8'>
+        <div className='block sm:hidden'>
+          <HamburgerMenu
+            isMobileMenuOpen={isMobileMenuOpen}
+            setIsMobileMenuOpen={setIsMobileMenuOpen}
+          />
+          {isMobileMenuOpen && <MobileMenu />}
+        </div>
+        <DesktopLeftMenu />
+        <NavLink to={"/"}>
+          <h3 className='font-bold'>PANGAEA</h3>
+        </NavLink>
+        <DesktopRightMenu toggleCartSidebar={toggleCartSidebar} />
+      </div>
+    </nav>
+  );
+}
+
+export default NavigationBar;

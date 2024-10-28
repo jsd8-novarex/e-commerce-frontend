0953ea15d2914d76dad02f43b6955f257518fc12
Zombox <ghostzb@outlook.com>
2024-10-29 00:13:19 +0700

    create(earth/MobileMenu)

---
+++ src/components/navigation/HamburgerMenu.tsx
@@ -0,0 +1,23 @@
+type HamburgerMenuPropsType = {
+  isMobileMenuOpen: boolean;
+  setIsMobileMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+function HamburgerMenu({ isMobileMenuOpen, setIsMobileMenuOpen }: HamburgerMenuPropsType) {
+  return (
+    <label htmlFor='hamburgerMenu' className='z-10 flex cursor-pointer flex-col p-2'>
+      <input
+        id='hamburgerMenu'
+        type='checkbox'
+        checked={isMobileMenuOpen}
+        onChange={() => setIsMobileMenuOpen((perv) => !perv)}
+        className='peer hidden'
+      />
+      <span className='hamburger-span w-2.5 origin-bottom peer-checked:translate-x-0.5 peer-checked:translate-y-0.5 peer-checked:rotate-45' />
+      <span className='hamburger-span w-5 origin-top peer-checked:-rotate-45' />
+      <span className='hamburger-span w-3 origin-bottom peer-checked:w-1/2 peer-checked:-translate-y-[4px] peer-checked:translate-x-[7px] peer-checked:rotate-45' />
+    </label>
+  );
+}
+
+export default HamburgerMenu;

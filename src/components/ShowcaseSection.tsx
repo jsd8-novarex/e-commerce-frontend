baa732409457bbd4657819aa0ee5e8315fdd249c
Zombox <ghostzb@outlook.com>
2024-10-29 00:05:24 +0700

    fix(earth/ShowcaseSection,HomePage):edit height and z-index

--- src/components/ShowcaseSection.tsx
+++ src/components/ShowcaseSection.tsx
@@ -7,7 +7,7 @@ type ShowCaseSectionPropsType = {
 function ShowcaseSection({ data }: ShowCaseSectionPropsType) {
   return (
     <section className='relative h-full w-full snap-center snap-always'>
-      <img src={data.url} alt={data.name} className='-z-10 h-full w-full' />
+      <img src={data.url} alt={data.name} className='-z-[2] h-full w-full ' />
       <div className='absolute inset-0 z-0 h-full w-full bg-black/25' />
       <div className='absolute bottom-[12%] left-[5%] z-10 w-72 p-4 text-white sm:w-96 xl:w-[32rem]'>
         <p className='text-xl xl:text-2xl'>{data.name}</p>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import { ShowCaseSectionData } from "../constraints/SHOWCASE_DATA";
 
 function HomePage() {
   return (
-    <main className='hide-scroll h-[91.7vh] w-dvw snap-y snap-mandatory overflow-hidden overflow-y-auto scroll-smooth ease-in-out'>
+    <main className='hide-scroll h-[90.4vh] w-full snap-y snap-mandatory overflow-hidden overflow-y-auto scroll-smooth ease-in-out xl:h-[91vh] 2xl:h-[91.2vh]'>
       {ShowCaseSectionData.map((data, index) => (
         <ShowcaseSection key={index} data={data} />
       ))}

bddc6c7ae858743ea19997e84e7db05b756f013b
Zombox <ghostzb@outlook.com>
2024-11-04 21:39:24 +0700

    fix(earth/CollectionsPage ) edit css grid

--- src/pages/CollectionsPage.tsx
+++ src/pages/CollectionsPage.tsx
@@ -1,10 +1,10 @@
-import CollectionsFilterBar from "../components/collectionsPage/CollectionsFilterBar";
-import ProductCard from "../components/collectionsPage/ProductCard";
+import CollectionsFilterBar from "../components/collections/CollectionsFilterBar";
+import ProductCard from "../components/collections/ProductCard";
 
 function CollectionsPage() {
   return (
-    <main className='relative grid min-h-screen grid-cols-12 gap-y-4 pt-32'>
-      <section className='col-span-full px-4 sm:px-8'>
+    <main className='relative grid min-h-screen grid-cols-12 gap-y-4 px-5 pt-32 sm:px-10'>
+      <section className='col-span-full'>
         <div className='mb-8'>
           <p>Home / Shop All</p>
         </div>
@@ -18,9 +18,9 @@ function CollectionsPage() {
         </div>
       </section>
       <CollectionsFilterBar />
-      <section className='col-span-full px-4 py-8 sm:px-8'>
+      <section className='col-span-full'>
         <div className='grid grid-cols-12 gap-4 justify-self-center'>
-          {Array.from({ length: 20 }).map((_, index) => (
+          {Array.from({ length: 10 }).map((_, index) => (
             <div key={index} className='col-span-full sm:col-span-6 lg:col-span-3'>
               <ProductCard />
             </div>

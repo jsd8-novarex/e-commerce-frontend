08ef85dd75214f00e4718c32b390abb004ffad94
Zombox <ghostzb@outlook.com>
2024-11-04 21:28:00 +0700

    fix(earth/Backdrop) edit for format

--- src/constraints/SHOWIMAGE_DATA.ts
+++ src/constraints/SHOWIMAGE_DATA.ts
@@ -1,39 +1,38 @@
 export type ShowImageProductDataType = {
-    name: string;
-    information: string;
-    url: string;
-    alternate: string;
+  name: string;
+  information: string;
+  url: string;
+  alternate: string;
 };
 
 export type ShowImageProductType = ShowImageProductDataType[];
 
 export const ShowImageProductData: ShowImageProductType = [
-    {
-        name: "Mens Recycled Cashmere Cardigan - Black",
-        information: 
-        "Top left",
-        url: "https://pangaia.com/cdn/shop/files/Cashmere-Cardigan-Black-1.png?crop=center&height=1023&v=1727177408&width=768",
-        alternate:"https://cdn.prod.marmalade.co/products/fit-in/640x640/filters:quality(80):fill(ffffff):watermark(overlays/overlay-black.webp,repeat,repeat,98)/www.pangaia.com%2Fproducts%2Frecycled-cashmere-cardigan-black%2F1662476027%2FCashmere-Cardigan-Black-1.png",
-    },
-    {
-        name: "Mens Recycled Cashmere Cardigan - Black",
-        information: 
-        "Top right",
-        url: "https://pangaia.com/cdn/shop/files/Cashmere-Cardigan-Black-Male-1.jpg?crop=center&height=1023&v=1727177408&width=768",
-        alternate:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSQ4TbpCK67XVYrY4nbApJa9Cs4VXLQEd_-iMghd7iY4EyqB5NT",
-    },
-    {
-        name: "Mens Recycled Cashmere Cardigan - Black",
-        information: 
-        "bottom left",
-        url: "https://pangaia.com/cdn/shop/files/Cashmere-Cardigan-Black-Male-2.jpg?crop=center&height=1023&v=1727177408&width=768",
-        alternate:"https://i.pinimg.com/236x/3c/66/bd/3c66bd4dfb9c803dec8eac5f1b5ac9a9.jpg",
-    },{
-        name: "Mens Recycled Cashmere Cardigan - Black",
-        information: 
-        "bottom right",
-        url: "https://pangaia.com/cdn/shop/files/Cashmere-Cardigan-Black-2.jpg?crop=center&height=1023&v=1727177408&width=768",
-        alternate:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQhXJ5XagIJ6GZH01d3Jbmzcym6Gp2ndzDRAGonu5lYMYMxug6I",
-    },
+  {
+    name: "Mens Recycled Cashmere Cardigan - Black",
+    information: "Top left",
+    url: "https://pangaia.com/cdn/shop/files/Cashmere-Cardigan-Black-1.png?crop=center&height=1023&v=1727177408&width=768",
+    alternate:
+      "https://cdn.prod.marmalade.co/products/fit-in/640x640/filters:quality(80):fill(ffffff):watermark(overlays/overlay-black.webp,repeat,repeat,98)/www.pangaia.com%2Fproducts%2Frecycled-cashmere-cardigan-black%2F1662476027%2FCashmere-Cardigan-Black-1.png",
+  },
+  {
+    name: "Mens Recycled Cashmere Cardigan - Black",
+    information: "Top right",
+    url: "https://pangaia.com/cdn/shop/files/Cashmere-Cardigan-Black-Male-1.jpg?crop=center&height=1023&v=1727177408&width=768",
+    alternate:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSQ4TbpCK67XVYrY4nbApJa9Cs4VXLQEd_-iMghd7iY4EyqB5NT",
+  },
+  {
+    name: "Mens Recycled Cashmere Cardigan - Black",
+    information: "bottom left",
+    url: "https://pangaia.com/cdn/shop/files/Cashmere-Cardigan-Black-Male-2.jpg?crop=center&height=1023&v=1727177408&width=768",
+    alternate: "https://i.pinimg.com/236x/3c/66/bd/3c66bd4dfb9c803dec8eac5f1b5ac9a9.jpg",
+  },
+  {
+    name: "Mens Recycled Cashmere Cardigan - Black",
+    information: "bottom right",
+    url: "https://pangaia.com/cdn/shop/files/Cashmere-Cardigan-Black-2.jpg?crop=center&height=1023&v=1727177408&width=768",
+    alternate:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQhXJ5XagIJ6GZH01d3Jbmzcym6Gp2ndzDRAGonu5lYMYMxug6I",
+  },
 ];
-

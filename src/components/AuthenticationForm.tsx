a7bc76672a549dbb68f09bbce4b3e3bd11d93ad3
Zombox <ghostzb@outlook.com>
2024-11-04 21:42:55 +0700

    fix(earth/AuthenticationForm,SignInAdminPage,SignInPage, SignUpPage) edit design part1

--- src/components/AuthenticationForm.tsx
+++ src/components/AuthenticationForm.tsx
@@ -3,9 +3,10 @@ import { PropsWithChildren } from "react";
 function AuthenticationForm({ children }: PropsWithChildren) {
   return (
     <div className='flex h-dvh w-dvw items-center justify-center bg-white md:bg-stone-100'>
-      <div className='flex h-2/3 w-dvw flex-col justify-center bg-white p-10 md:w-1/2 lg:w-1/3 xl:w-1/3'>
-        <div className='mb-6'>
-          <h1 className='text-center text-4xl font-bold md:text-5xl'>Pangaea</h1>
+      <div className='flex h-2/3 w-full flex-col justify-center gap-y-12 bg-white p-4 sm:justify-evenly sm:gap-y-4 sm:p-10 md:w-1/2 lg:w-1/3'>
+        <div className='mb-4 flex flex-col items-center gap-4 text-2xl font-bold'>
+          <h1 className='text-center text-4xl font-bold uppercase md:text-5xl'>Pangaea</h1>
+          <h2>Create an Account</h2>
         </div>
         {children}
       </div>
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -4,34 +4,36 @@ import AuthenticationForm from "../components/AuthenticationForm";
 function SignInPage() {
   return (
     <AuthenticationForm>
-      <form className='mt-8 flex flex-col gap-y-6'>
-        <input
-          type='email'
-          placeholder='Enter your email'
-          className='auth-form__input'
-          minLength={8}
-          required
-        />
+      <div>
+        <form className='mt-8 flex flex-col gap-y-6'>
+          <input
+            type='email'
+            placeholder='Enter your email'
+            className='auth-form__input'
+            minLength={8}
+            required
+          />
 
-        <input
-          type='password'
-          placeholder='Enter your password'
-          className='auth-form__input'
-          minLength={8}
-          required
-        />
+          <input
+            type='password'
+            placeholder='Enter your password'
+            className='auth-form__input'
+            minLength={8}
+            required
+          />
 
-        <div className='flex justify-center'>
-          <button type='submit' className='auth-form__btn'>
-            LOGIN
-          </button>
+          <div className='flex justify-center'>
+            <button type='submit' className='auth-form__btn'>
+              LOGIN
+            </button>
+          </div>
+        </form>
+        <div className='mt-2 text-right hover:text-gray-400'>
+          <Link to='/'>Forgot Password?</Link>
         </div>
-      </form>
-      <div className='mt-2 text-right hover:text-gray-400'>
-        <Link to='/'>Forgot Password?</Link>
-      </div>
-      <div className='mt-5 py-5 text-center underline hover:text-gray-400 md:text-xl'>
-        <Link to='/'>Create new account</Link>
+        <div className='mt-5 py-5 text-center underline hover:text-gray-400 md:text-xl'>
+          <Link to='/sign-up'>Create new account</Link>
+        </div>
       </div>
     </AuthenticationForm>
   );

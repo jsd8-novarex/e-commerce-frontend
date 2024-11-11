9d0004030afb485c35c59b2fa51bd97165321f02
Zombox <ghostzb@outlook.com>
2024-11-11 11:09:39 +0700

    fix(earth/NavigationBar,HamburgerMenu,MobileMenu ) : edit z-index

--- src/components/navigation/MobileMenu.tsx
+++ src/components/navigation/MobileMenu.tsx
@@ -3,7 +3,8 @@ import { navigationData } from "../../constraints/NAVIGATION_DATA";
 
 function MobileMenu() {
   return (
-    <ul className='absolute inset-0 -z-10 flex h-dvh w-full flex-col items-center justify-center gap-y-2 bg-white'>
+    <div className="absolute inset-0 z-[1] h-dvh w-full">
+      <ul className=' flex flex-col h-full w-full items-center justify-center gap-y-2 bg-white'>
       {navigationData.map((item) => (
         <li key={item.order} className='w-full text-center'>
           <NavLink to={item.path} className='block h-full w-full p-4 uppercase'>
@@ -12,6 +13,7 @@ function MobileMenu() {
         </li>
       ))}
     </ul>
+    </div>
   );
 }
 
--- src/components/navigation/NavigationBar.tsx
+++ src/components/navigation/NavigationBar.tsx
@@ -1,21 +1,31 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import HamburgerMenu from "./HamburgerMenu";
 import MobileMenu from "./MobileMenu";
 import DesktopLeftMenu from "./DesktopLeftMenu";
 import DesktopRightMenu from "./DesktopRightMenu";
+import clsx from "clsx";
 
 type NavigationBarPropsType = {
   toggleCartSidebar: () => void;
 };
 
 function NavigationBar({ toggleCartSidebar }: NavigationBarPropsType) {
+  const location = useLocation();
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
+
+  const isHome = location.pathname === "/";
 
   return (
     <nav className='fixed top-0 z-10 w-full'>
-      <div className='relative flex items-center justify-between bg-white px-4 py-5 md:px-8'>
-        <div className='block sm:hidden'>
+      <div
+        className={clsx("relative flex items-center justify-between px-4 py-5 md:px-8", {
+          "bg-transparent": isHome === true,
+          "bg-black": isHome === false,
+        })}
+      >
+        <div className=' block sm:hidden'>
+          <div className=" block h-10 w-10 sm:hidden"/>
           <HamburgerMenu
             isMobileMenuOpen={isMobileMenuOpen}
             setIsMobileMenuOpen={setIsMobileMenuOpen}
@@ -24,7 +34,7 @@ function NavigationBar({ toggleCartSidebar }: NavigationBarPropsType) {
         </div>
         <DesktopLeftMenu />
         <NavLink to={"/"}>
-          <h3 className='font-bold uppercase'>Shining</h3>
+          <h3 className='font-bold uppercase text-white'>Shining</h3>
         </NavLink>
         <DesktopRightMenu toggleCartSidebar={toggleCartSidebar} />
       </div>

bb50d7a4ad9c292183297f3de9ec4939d4c97401
Zombox <ghostzb@outlook.com>
2024-11-13 17:20:19 +0700

    add(earth/SignInPage, useAuthentication) : create useAuthentication and add function v1

---
+++ src/hook/user/useAuthentication.ts
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { handleInputObjectFieldChange } from "../../helpers/utils";
+import { useNavigate } from "react-router-dom";
+
+type DataForVerifyType = {
+  email: string;
+  password: string;
+};
+
+function useAuthentication() {
+  const navigate = useNavigate();
+
+  const [authStatus, setAuthStatus] = useState<string>("");
+  const [isProcessing, setIsProcessing] = useState<boolean>(false);
+  const [dataForVerify, setDataForVerify] = useState<DataForVerifyType>({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    handleInputObjectFieldChange<DataForVerifyType>(event, setDataForVerify);
+  };
+
+  const handleAuthentication = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setIsProcessing(true);
+    setAuthStatus("Verifying user information...");
+
+    if (dataForVerify.email === "earth@gamil.com" && dataForVerify.password === "12345678") {
+      setAuthStatus("Successfully authenticated.");
+    } else {
+      setAuthStatus("Invalid credentials. Please try again.");
+      setTimeout(() => {
+        navigate("/", { replace: true });
+      }, 500);
+    }
+    setIsProcessing(false);
+  };
+
+  return {
+    authStatus,
+    isProcessing,
+    dataForVerify,
+    handleInputChange,
+    handleAuthentication,
+  };
+}
+
+export default useAuthentication;
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,11 +1,15 @@
 import { NavLink } from "react-router-dom";
 import AuthenticationForm from "../components/AuthenticationForm";
+import useAuthentication from "../hook/user/useAuthentication";
 
 function SignInPage() {
+  const { isProcessing, dataForVerify, handleInputChange, handleAuthentication } =
+    useAuthentication();
+
   return (
     <AuthenticationForm status='sign-in' message='Enter your account details'>
       <div className='row-start-3 row-end-7 flex flex-col items-center justify-center'>
-        <form className='flex min-w-72 flex-col gap-y-4 sm:w-96'>
+        <form onSubmit={handleAuthentication} className='flex min-w-72 flex-col gap-y-4 sm:w-96'>
           <div className='flex flex-col'>
             <label htmlFor='email' className='auth-form__label'>
               Email
@@ -14,9 +18,11 @@ function SignInPage() {
               id='email'
               type='email'
               placeholder='Enter your email'
-              className='auth-form__input'
-              minLength={8}
+              value={dataForVerify.email}
+              onChange={handleInputChange}
+              disabled={isProcessing}
               required
+              className='auth-form__input'
             />
           </div>
           <div className='flex flex-col'>
@@ -27,13 +33,16 @@ function SignInPage() {
               id='password'
               type='password'
               placeholder='Enter your password'
-              className='auth-form__input'
+              value={dataForVerify.password}
               minLength={8}
+              onChange={handleInputChange}
+              disabled={isProcessing}
               required
+              className='auth-form__input'
             />
           </div>
           <div className='flex flex-col justify-center'>
-            <button type='submit' className='auth-form__btn uppercase'>
+            <button type='submit' disabled={isProcessing} className='auth-form__btn uppercase'>
               sign in
             </button>
             <NavLink to='/' className='mt-2 text-right hover:text-gray-400'>

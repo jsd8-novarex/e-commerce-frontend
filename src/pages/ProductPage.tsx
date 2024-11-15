507e089dec5e1bc6a9b9dbe94c045d32e7f1736f
sirinda <sirinda.bdev@gmail.com>
2024-11-15 17:15:44 +0700

    Edit(mine/productpage):1. edit props 2.delete line

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -2,7 +2,6 @@ import ProductImages from "../components/product/ProductImages";
 import ProductDetail from "../components/product/ProductDetail";
 import { ShowImageProductData2 } from "../constraints/SHOWIMAGE_DATA";
 
-
 function ProductPage() {
   return (
     <div className='grid h-full w-full gap-3 py-32 sm:grid-cols-12 sm:px-10'>

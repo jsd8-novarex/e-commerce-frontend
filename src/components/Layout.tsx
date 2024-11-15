10a57984baf2044976fdce5fca6a56a8caf92063
Zombox <ghostzb@outlook.com>
2024-11-15 17:50:43 +0700

    add(earth/Layout) : add useScrollLock

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,29 +1,17 @@
-import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import useScrollLock from "../hook/useScrollLock";
 import NavigationBar from "./navigation/NavigationBar";
 import CartSidebar from "./CartSidebar";
-import { useScrollLockStore } from "../store/scrollLock.store";
 // import Footer from "./Footer";
 
 function Layout() {
-  const isPageScrollLocked = useScrollLockStore((state) => state.isPageScrollLocked);
 
-  useEffect(() => {
-    if (isPageScrollLocked) {
-      document.body.style.overflowY = "hidden ";
-    } else {
-      document.body.style.overflowY = "auto";
-    }
-
-    return () => {
-      document.body.style.overflowY = "auto";
-    };
-  }, [isPageScrollLocked]);
+  useScrollLock()
 
   return (
     <div className='relative flex h-full flex-col bg-white'>
       <NavigationBar />
-      <CartSidebar isCartSidebarOpen={isPageScrollLocked} />
+      <CartSidebar/>
       <Outlet />
       {/* <Footer /> */}
     </div>

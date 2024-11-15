2cc26c7ff25ef862f51bc1fa8fe70489c1476631
sirinda <sirinda.bdev@gmail.com>
2024-11-15 17:15:12 +0700

    Edit(mine/productpage):1. edit props

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,11 +1,12 @@
 import ProductImages from "../components/product/ProductImages";
 import ProductDetail from "../components/product/ProductDetail";
-import { ShowImageProductData } from "../constraints/SHOWIMAGE_DATA";
+import { ShowImageProductData2 } from "../constraints/SHOWIMAGE_DATA";
+
 
 function ProductPage() {
   return (
     <div className='grid h-full w-full gap-3 py-32 sm:grid-cols-12 sm:px-10'>
-      <ProductImages data={ShowImageProductData} />
+      <ProductImages data={ShowImageProductData2} />
       <ProductDetail />
     </div>
   );

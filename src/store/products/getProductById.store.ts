011d4a724e7237faecc4ea7b1362cc5030ffdf72
sirinda <sirinda.bdev@gmail.com>
2024-12-06 11:21:07 +0700

    Create(mine/ProductbyId)1.service 2.store 3.hook

---
+++ src/store/products/getProductById.store.ts
@@ -0,0 +1,18 @@
+import { create } from "zustand";
+import getProductById from "../../service/products/getProductById";
+import { ProductType } from "../../service/products/getProduct.type";
+
+interface GetProductByIdStateType {
+  data: ProductType | null;
+  error: string | null;  
+  fetchProductsData: (productId: string) => Promise<void>;
+}
+
+export const useGetProductByIdStore = create<GetProductByIdStateType>((set) => ({
+  data: null,
+  error: null,  
+  fetchProductsData: async (productId: string) => {
+    const [response, error] = await getProductById(productId);
+    set({ data: response, error: error });
+  },
+}));

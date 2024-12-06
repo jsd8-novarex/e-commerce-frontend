65acaae303b2d5f1a7ba27d59d92478e6af5d20b
sirinda <sirinda.bdev@gmail.com>
2024-12-06 11:22:28 +0700

    Create(mine/ProductType)

---
+++ src/service/products/getProduct.type.ts
@@ -0,0 +1,34 @@
+export interface ProductType {
+  status: string;
+  products: ProductDataType[];
+}
+
+export interface ProductDataType {
+  _id: string;
+  name: string;
+  brand: string;
+  category: string;
+  gender: string;
+  description: string;
+  product_choices: ProductChoiceType[];
+  create_timestamp: Date;
+  last_updated_timestamp: Date;
+  creator_id: string;
+  last_op_id: string;
+  tram_status: boolean;
+}
+
+export interface ProductChoiceType {
+  id?: string;
+  color: string;
+  size: string;
+  price: number;
+  sku: string;
+  quantity: number;
+  images: ProductImageType[];
+}
+
+export interface ProductImageType {
+  url: string;
+  index: number;
+}

bdeb7aa723bf9b5fff6d28d2978f1d5361bb8d62
sirinda <sirinda.bdev@gmail.com>
2024-12-11 17:51:46 +0700

    Edit(mine/getAllProduct)

--- src/hook/products/useGetAllProduct.ts
+++ src/hook/products/useGetAllProduct.ts
@@ -8,7 +8,7 @@ function useGetAllProduct(filter: string) {
     fetchProductsData(filter);
   }, [fetchProductsData, filter]);
 
-  return { data, error, isLoading };
+  return { data, error };
 }
 
 export default useGetAllProduct;
--- src/store/products/getAllProduct.store.ts
+++ src/store/products/getAllProduct.store.ts
@@ -4,14 +4,20 @@ import { ProductType } from "../../service/products/getProduct.type";
 
 interface GetAllProductStateType {
   data: ProductType | null;
-  error: string | null;
+  error: string | null;  
   fetchProductsData: (gender?: string) => Promise<void>;
 }
 
 export const useGetAllProductStore = create<GetAllProductStateType>((set) => ({
   data: null,
   error: null,
-  fetchProductsData: async (gender?: string) => {
+  isLoading: false,
+  fetchProductsData: async (gender?: string) => {    
+    if (gender === undefined ) {
+      set({ data: null, error: null });
+      return;
+    } 
+    set({ error: null })   
     const [response, error] = await getAllProduct(gender);
     set({ data: response, error: error });
   },
